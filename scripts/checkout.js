4144252a2f2cd18a4f3472cf847b29e0d6f7e156
Olti Haxhiavdyli <oltihaxhiavdyli1@gmail.com>
2023-08-08 18:28:52 +0200

    s

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,8 +16,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-    <div class="cart-item-container
-      js-cart-item-container-${matchingProduct.id}">
+    <div class="cart-item-container">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -95,8 +94,7 @@ cart.forEach((cartItem) => {
   `;
 });
 
-document.querySelector('.js-order-summary')
-  .innerHTML = cartSummaryHTML;
+document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
 document.querySelectorAll('.js-delete-link')
   .forEach((link) => {
