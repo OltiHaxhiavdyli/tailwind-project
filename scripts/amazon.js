66dcf241fc6d9cade231529726218356b7fc720b
Olti Haxhiavdyli <oltihaxhiavdyli1@gmail.com>
2023-08-08 14:28:19 +0200

    added more modules

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from '../data/cart.js';
+import { cart, addToCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 
 let productsHTML = '';
@@ -58,37 +58,22 @@ products.forEach(product => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-function addToCart() {
+function updateCartQuantity() {
+  let cartQuantity = 0;
 
+  cart.forEach(cartItem => {
+    cartQuantity += cartItem.quantity;
+  })
+
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 }
 
 document.querySelectorAll('.js-add-to-cart-button').forEach(button => {
   button.addEventListener('click', () => {
-    const { productId } = button.dataset;
+    const productId = button.dataset;
 
-    let matchingItem;
+    addToCart(productId);
 
-    cart.forEach(item => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    })
-
-    if (matchingItem) {
-      matchingItem.quantity += quantity;
-    } else {
-      cart.push({
-        productId,
-        quantity
-      })
-    }
-
-    let cartQuantity = 0;
-
-    cart.forEach(item => {
-      cartQuantity += item.quantity;
-    })
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    updateCartQuantity();
   });
 })
